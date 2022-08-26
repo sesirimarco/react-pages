28427470e3b9a546794555266e131bb7fd6ffad6
wind <sesirimarco@gmail.com>
2022-08-25 22:08:11 -0300

    add change

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and \_(o-0)-/
-        </p>
+        <p>\_(o-0)-/</p>
         <a
           className='App-link'
           href='https://reactjs.org'
