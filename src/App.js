34a007c0170edebc924a29ed5e120790f0efd058
sesirimarco <sesirimarco@gmail.com>
2022-08-29 15:33:55 -0300

    add changes (#4)
    * add change
    
    * add gh-pages
    
    * add change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        <p>Test github pages deploy</p>
         <p>\_(o-0)-/</p>
         <a
           className='App-link'
