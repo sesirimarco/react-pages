e3f58ac8d3c2440c41c68cc0c9d117611885f3a8
sesirimarco <sesirimarco@gmail.com>
2022-08-29 19:59:17 -0300

    add changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <p>\_(*-*)-/</p>
         <p>\_(o-0)-/</p>
+        <p>Some more changes</p>
         <a
           className='App-link'
           href='https://reactjs.org'
